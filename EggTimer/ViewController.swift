010468d6ae52e678887b4d60f831e1911ca9ead1
Damian <damian.jahn@icloud.com>
2020-03-31 17:24:41 +0200

    Initial commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Damian Jahn on 31/03/2020.
+//  Copyright © 2020 Damian Jahn. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+
+}
